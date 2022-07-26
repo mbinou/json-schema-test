92e3090654f227c55bc4d4967cac79aa357b9267
Munehito BINOU <onib.muu@gmail.com>
2022-07-26 12:54:38 +0900

    feat: index

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from json_schema_for_humans.generation_configuration import GenerationConfigurat
 
 config = GenerationConfiguration(copy_css=False, expand_buttons=True)
 
-generate_from_filename("my_schema.json", "schema_doc.html", config=config)
+generate_from_filename("my_schema.json", "index.html", config=config)
